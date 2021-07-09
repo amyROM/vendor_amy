07b9c19cd84eb376c1692ef95c4e123ec34fc9ff
Amy <amy@blisslabs.org>
2021-07-09 19:42:48 +0000

    Drop Lineage Updater
    Change-Id: I3324185d97b7bf608a7cd8bf722db62df83b5ccd

--- config/common.mk
+++ config/common.mk
@@ -114,8 +114,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     LineageParts \
     LineageSettingsProvider \
-    LineageSetupWizard \
-    Updater
+    LineageSetupWizard
 
 # Themes
 PRODUCT_PACKAGES += \
