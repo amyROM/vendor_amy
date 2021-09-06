32598bd376fef33515482099f3363bdba747083d
Nick <nift4@protonmail.com>
2021-09-06 15:49:56 +0300

    amy: Unlock blur for everyone
    * flag now controls default value
    
    Signed-off-by: Wind/owZ <windowz414@gnuweeb.org>

--- config/common_full.mk
+++ config/common_full.mk
@@ -7,6 +7,16 @@ PRODUCT_SIZE := full
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
 
+# Blur
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
+
+ifneq ($(TARGET_USES_BLUR),true)
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.sf.disable_blurs=1
+endif
+
 # Fonts
 PRODUCT_PACKAGES += \
     fonts_customization.xml \
