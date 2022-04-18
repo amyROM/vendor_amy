1769cd27adac1f283ce9604c4f51705e2de29018
Amy <amy@amyrom.ml>
2022-04-18 15:33:09 +0100

    amyROM bringup
    Change-Id: Iab322e4344e2e079ca34a7fe431a67ead179ff6e

--- core/config.mk
+++ core/config.mk
@@ -318,7 +318,7 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
 ifneq ($(LINEAGE_BUILD),)
-include vendor/lineage/config/BoardConfigLineage.mk
+include vendor/amy/config/BoardConfigLineage.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
