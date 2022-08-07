1d68df4878807d193c9b4b08f3e75a95fb5d8a08
LuK1337 <priv.luk@gmail.com>
2022-08-07 21:30:09 +0200

    config: Exclude LatinIME dictionaries from RRO overlays
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/4991
    Change-Id: I7722ac86d9d3f02f393b2c120341db5a2b648ff5
    (cherry picked from commit 7551e9ecddec2ce197b7ae48ac4767a55288f32f)

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -7,5 +7,6 @@ PRODUCT_PACKAGES += \
 
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/amy/overlay/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/amy/overlay/dictionaries
 
 $(call inherit-product, vendor/amy/config/telephony.mk)
--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -7,3 +7,4 @@ PRODUCT_PACKAGES += \
 
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/amy/overlay/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/amy/overlay/dictionaries
