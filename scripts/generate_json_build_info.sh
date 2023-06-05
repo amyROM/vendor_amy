1938b05bf3d0d8fc6e1bfb16a6be033b6a624502
Amy King <amy@amyrom.tech>
2023-06-05 13:42:02 +0100

    Fix OTA json for reborn

--- scripts/generate_json_build_info.sh
+++ scripts/generate_json_build_info.sh
@@ -6,11 +6,11 @@ then
   DEVICE=$(echo $TARGET_PRODUCT | cut -d "_" -f2)
   if [ -f $file_path ]; then
     file_size=$(stat -c%s $file_path)
-    id=$(cat "$file_path.md5sum" | cut -d' ' -f1)
+    id=$(cat "$file_path.sha256sum" | cut -d' ' -f1)
     datetime=$(grep ro\.build\.date\.utc ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
     rom_version=$(grep ro\.lineage\.version ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2 | cut -d "v" -f2);
     custom_build_type=$(grep ro\.lineage\.releasetype ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
-    echo "{\n  \"response\": [\n    {\n      \"datetime\": $datetime,\n      \"filename\": \"$file_name\",\n      \"id\": \"$id\",\n      \"romtype\": \"$custom_build_type\",\n      \"size\": $file_size,\n      \"url\": \"https://memediatek.00p513.workers.dev/amyROM/$DEVICE/$file_name\",\n      \"version\": \"$rom_version\"\n    }\n  ]\n}" > $file_path.json
+    echo "{\n  \"response\": [\n    {\n      \"datetime\": $datetime,\n      \"filename\": \"$file_name\",\n      \"id\": \"$id\",\n      \"romtype\": \"$custom_build_type\",\n      \"size\": $file_size,\n      \"url\": \"https://sabina.amyrom.tech/ota/reborn/$DEVICE/$file_name\",\n      \"version\": \"$rom_version\"\n    }\n  ]\n}" > $file_path.json
     echo "OTA JSON: $file_path.json"
   fi
 fi
